b2ef8cf27108ccbf12fc5482db69e7b73fa5254e
Adriana <150440061+Adrianaa911@users.noreply.github.com>
2025-10-08 10:06:50 +0100

    Update Tree.java

--- Tree.java
+++ Tree.java
@@ -1,1 +1,27 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    // introduce ourself and print a tree
+    System.out.println("Hey there, I'm Adriana and I'm learning to code in Java!");
+    System.out.println("I’m going to plant a tree today!");
+    System.out.println("Ready to get my hands dirty!");
 
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");   
+
+    System.out.println("  **  ");
+    System.out.println(" ***** ");
+    System.out.println(" ******* ");
+    System.out.println("**** ** *****");
+    System.out.println("**************");   
+    System.out.println("******** * *********");
+    System.out.println("***********************");
+    System.out.println("  *************  ");
+    System.out.println("  ******  ");
+
+  }
+
+}
